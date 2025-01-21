906d53b8c022ad3627aabf0e82348cd092ddd8a5
patz <147352311+patzsl@users.noreply.github.com>
2025-01-21 16:51:14 -0300

    feat: adicionar suporte ao TypeORM e configuração do banco de dados
    - Adicionadas dependências: mysql, reflect-metadata e typeorm no package.json e package-lock.json.
    - Configuração do TypeORM no arquivo index.ts para conectar ao banco de dados usando variáveis de ambiente.
    - Adicionada a biblioteca dotenv para gerenciar variáveis de ambiente.

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,40 @@ import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
 import "./socket";
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:4200",
-      "http://localhost:5000",
-    ],
-  })
-);
+const dataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ["src/entities/*.ts"],
+  logging: false,
+  synchronize: true,
+});
 
-routes(app);
+dataSource.initialize().then((connection) => {
+  const app = express();
 
-app.listen(8000, () => {
-  console.log("listening to port 8000");
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:4200",
+        "http://localhost:5000",
+      ],
+    })
+  );
+
+  routes(app);
+
+  app.listen(8000, () => {
+    console.log("listening to port 8000");
+  });
 });
