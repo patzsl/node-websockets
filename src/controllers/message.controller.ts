37b3ff0ac1a57c33089d89ff9f7e8aefc1bb665e
patz <147352311+patzsl@users.noreply.github.com>
2025-01-21 21:52:22 -0300

    feat: adicionar funcionalidade para listar mensagens
    - Implementada a função Messages em src/controllers/message.controller.ts para buscar e retornar mensagens enviadas e recebidas pelo usuário autenticado, com suporte à paginação.
    - Atualizada a rota GET /api/messages em src/routes.ts para permitir a listagem de mensagens autenticadas.

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -1,6 +1,23 @@
 import { Message } from "../entities/message.entity";
 import { io } from "../socket";
 
+export const Messages = async (req, res) => {
+  const user = req["user"];
+  const take = 10;
+  const page = parseInt(req.query.page || "1");
+  const [messages, total] = await Message.findAndCount({
+    where: [{ sender: { id: user.id } }, { receiver: { id: user.id } }],
+    relations: ["sender", "receiver"],
+    skip: (page - 1) * take,
+    take,
+  });
+
+  res.send({
+    messages,
+    total,
+  });
+};
+
 export const SendMessage = async (req, res) => {
   const user = req["user"];
 
--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ import {
 } from "./controllers/auth.controller";
 import { AuthMiddleware } from "./middlewares/auth.middleware";
 import { Users } from "./controllers/user.controller";
-import { SendMessage } from "./controllers/message.controller";
+import { Messages, SendMessage } from "./controllers/message.controller";
 
 export const routes = (router: Router) => {
   router.post("/api/register", Register);
@@ -15,5 +15,6 @@ export const routes = (router: Router) => {
   router.get("/api/user", AuthMiddleware, GetUser);
   router.put("/api/user", AuthMiddleware, UpdateUser);
   router.get("/api/users", AuthMiddleware, Users);
+  router.get("/api/messages", AuthMiddleware, Messages);
   router.post("/api/messages", AuthMiddleware, SendMessage);
 };
